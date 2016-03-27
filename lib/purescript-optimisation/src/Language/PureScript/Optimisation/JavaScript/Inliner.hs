454635e530231e73703c1596a79854bd00a17f3c
Will Jones <will@sacharissa.co.uk>
2016-03-27 12:42:34 +0200

    Build a map of defined names.

--- lib/purescript-optimisation/src/Language/PureScript/Optimisation/JavaScript/Inliner.hs
+++ lib/purescript-optimisation/src/Language/PureScript/Optimisation/JavaScript/Inliner.hs
@@ -1,20 +1,34 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes      #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Language.PureScript.Optimisation.JavaScript.Inliner where
 
 import qualified Data.Generics              as G
+import qualified Data.Map.Strict            as M
+import qualified Data.Text                  as T
 import qualified Language.JavaScript.Parser as JS.P
 
-data Module
-  = Module
-      { mName :: !String
-      , mBody :: !JS.P.JSNode
+data Environment
+  = Environment
+      { eDecls :: !(M.Map QualifiedName JS.P.JSNode)
       }
 
   deriving (Eq, Show)
 
-jsModule :: G.GenericQ (Maybe Module)
-jsModule
+data QualifiedName
+  = QualifiedName
+      { qnModule  :: !T.Text
+      , qnName    :: !T.Text
+      }
+
+  deriving (Eq, Ord, Show)
+
+getAllDecls :: JS.P.JSNode -> M.Map QualifiedName JS.P.JSNode
+getAllDecls
+  = M.fromList . everyTopLevel getAllDeclsQ
+
+getAllDeclsQ :: G.GenericQ (Maybe [(QualifiedName, JS.P.JSNode)])
+getAllDeclsQ
   = G.mkQ Nothing go
   where
     go (JS.P.NN (JS.P.JSExpression
@@ -28,18 +42,42 @@ jsModule
             (JS.P.NN (JS.P.JSExpression
               [JS.P.NT (JS.P.JSStringLiteral _ modName) _ _])) _) : _) _)
 
-      : _)))
+      : _
+      )))
 
-      = Just Module
-          { mName = modName
-          , mBody = modBody
-          }
+      = Just (getModuleDecls (T.pack modName) modBody)
 
     go _
       = Nothing
 
-everyTopLevel :: G.GenericQ (Maybe r) -> G.GenericQ [r]
+getModuleDecls :: T.Text -> JS.P.JSNode -> [(QualifiedName, JS.P.JSNode)]
+getModuleDecls modName
+  = everyTopLevel getModuleDeclsQ
+  where
+    getModuleDeclsQ :: G.GenericQ (Maybe [(QualifiedName, JS.P.JSNode)])
+    getModuleDeclsQ
+      = G.mkQ Nothing go
+      where
+        go (JS.P.NN (JS.P.JSVarDecl (JS.P.NT (JS.P.JSIdentifier declName) _ _)
+          [_, declDef]))
+
+          = case declDef of
+              JS.P.NN (JS.P.JSMemberSquare _ _ _ _) ->
+                Nothing
+
+              _ ->
+                Just
+                  [ ( QualifiedName modName (T.pack declName)
+                    , declDef
+                    )
+
+                  ]
+
+        go _
+          = Nothing
+
+everyTopLevel :: G.GenericQ (Maybe [r]) -> G.GenericQ [r]
 everyTopLevel f x
   = case f x of
       Nothing -> concat (G.gmapQ (everyTopLevel f) x)
-      Just y  -> [y]
+      Just ys -> ys
---
+++ lib/purescript-optimisation/src/Language/PureScript/Optimisation/JavaScript/Types.hs
@@ -0,0 +1,1 @@
+module Language.PureScript.Optimisation.JavaScript.Types where
